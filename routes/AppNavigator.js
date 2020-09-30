a1d0fa492060743432c42b9ef251c248f780d26c
EddyVega-dev <evegag@fi.uba.ar>
2020-09-30 09:28:00 -0300

    sin modificar

---
+++ routes/AppNavigator.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from '../screens/Home';
+import Products from '../screens/Products';
+import Faq from '../screens/Faq'
+
+const Stack = createStackNavigator();
+
+export const AppNavigator = () => (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Products" component={Products} />
+      <Stack.Screen name="Preguntas Frecuentes" component={Faq} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
